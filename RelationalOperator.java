ae3fe7ce59939100fe17a14d03051afecbf69163
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-09-28 00:49:21 -0700

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,11 @@
+package Corejava;
+
+import java.util.Scanner;
+
+public class RelationalOperator {
+	//creating Scanner class to take input from user
+	public stactic void main(String[]args);
+	
+	
+
+}
